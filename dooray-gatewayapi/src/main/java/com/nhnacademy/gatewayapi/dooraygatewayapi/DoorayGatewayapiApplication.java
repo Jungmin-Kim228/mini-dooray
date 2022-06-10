f7c47915f97ad31922ac62e732bc563ff8ca26f3
dgb980228 <dgb980228@naver.com>
2022-06-10 14:37:36 +0900

    gateway 메인페이지 세팅

---
+++ dooray-gatewayapi/src/main/java/com/nhnacademy/gatewayapi/dooraygatewayapi/DoorayGatewayapiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.gatewayapi.dooraygatewayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorayGatewayapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoorayGatewayapiApplication.class, args);
+    }
+
+}
---
+++ dooray-gatewayapi/src/test/java/com/nhnacademy/gatewayapi/dooraygatewayapi/DoorayGatewayapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.gatewayapi.dooraygatewayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoorayGatewayapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
