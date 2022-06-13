3fd0d99958f20d78636fcccdc06b5b415503b402
dgb980228 <dgb980228@naver.com>
2022-06-13 11:17:32 +0900

    태스크 추가 폼

--- dooray-gatewayapi/src/main/java/com/nhnacademy/gatewayapi/dooraygatewayapi/controller/TaskController.java
+++ dooray-gatewayapi/src/main/java/com/nhnacademy/gatewayapi/dooraygatewayapi/controller/TaskController.java
@@ -1,6 +1,11 @@
 package com.nhnacademy.gatewayapi.dooraygatewayapi.controller;
 
+import com.nhnacademy.gatewayapi.dooraygatewayapi.domain.MilestoneDto;
+import com.nhnacademy.gatewayapi.dooraygatewayapi.domain.TagDto;
+import com.nhnacademy.gatewayapi.dooraygatewayapi.service.MilestoneService;
+import com.nhnacademy.gatewayapi.dooraygatewayapi.service.TagService;
 import com.nhnacademy.gatewayapi.dooraygatewayapi.service.TaskService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
@@ -14,11 +19,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class TaskController {
 
     private final TaskService taskService;
+    private final MilestoneService milestoneService;
+    private final TagService tagService;
 
     @GetMapping("/task/register/{projectNo}")
     public String taskRegisterForm(@PathVariable("projectNo") Integer projectNo,
                                Authentication authentication, Model model) {
         String userName = ((User) authentication.getPrincipal()).getUsername();
+        List<MilestoneDto> milestones = milestoneService.getMilestoneDtosByProjectNo(projectNo);
+        List<TagDto> tags = tagService.getTagDtosByProjectNo(projectNo);
+
+        model.addAttribute("milestones", milestones);
+        model.addAttribute("tags", tags);
         model.addAttribute("projectNo", projectNo);
         model.addAttribute("userName", userName);
         return "task/taskRegisterForm";
